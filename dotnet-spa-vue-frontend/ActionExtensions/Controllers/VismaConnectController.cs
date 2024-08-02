a917905b961962a4fc15b3244ef65cf10148995c
CPA Dennis Sulejman <a40168_cpa@visma.com>
2024-08-02 10:33:15 +0200

    Add VismaConnectController and Authentication properties to back-end

---
+++ dotnet-spa-vue-frontend/ActionExtensions/Controllers/VismaConnectController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionExtensions.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VismaConnectController : ControllerBase
+{
+    /// <summary>
+    /// This method is used to open a popup window that is used to verify that the user is authenticated to Visma Connect.
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    [HttpGet]
+    [Route("popuplogin")]
+    [Authorize]
+    public IActionResult PopupLogin()
+    {
+        return new ContentResult { Content = GetCallbackHtml(), ContentType = "text/html" };
+    }
+
+    /// <summary>
+    /// This method handles the callback required when confirming Visma Connect authentication.
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    [HttpPost]
+    [Route("callback")]
+    [AllowAnonymous]
+    public IActionResult Callback()
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// This method is used to handle the login failed scenario.
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    [HttpGet]
+    [Route("loginfailed")]
+    [AllowAnonymous]
+    public IActionResult LoginFailed()
+    {
+        return new ContentResult
+        {
+            // TODO: Add access denied HTML page
+        };
+    }
+
+    /// <summary>
+    /// This method is used to sign out the user.
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    [HttpGet]
+    [Route("signout")]
+    [AllowAnonymous]
+    public async Task<IActionResult> SignOutGet()
+    {
+        // TODO: Handle different environments
+        // For Production, this value is supposed to be: https://connect.visma.com
+        // The value below is for the staging environmentC
+        string vismaConnectOrigin = "https://connect.identity.stagaws.visma.com";
+
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        Response.Headers.Append(
+            "Content-security-policy",
+            $"frame-ancestors 'self' {vismaConnectOrigin}"
+        );
+        Response.Headers.Append("X-Frame-Options", $"allow-from {vismaConnectOrigin}");
+        return NoContent();
+    }
+
+    public string GetCallbackHtml()
+    {
+        return $@"
+            <!doctype html>
+            <html lang='en'>
+            <head>
+            <title>Visma Connect Authentication process</title>
+            <script>
+            function confirmLogin() {{
+                window.opener.popupConfirmation();
+                window.close();
+            }}
+            confirmLogin();
+            </script>
+            </head>
+            <body>
+                <p>Authenticating...</p>
+            </body>
+            </html>
+            ";
+    }
+}
--- dotnet-spa-vue-frontend/ActionExtensions/Program.cs
+++ dotnet-spa-vue-frontend/ActionExtensions/Program.cs
@@ -5,6 +5,74 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder
+    .Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie(setup =>
+    {
+        setup.Events.OnRedirectToAccessDenied = context =>
+        {
+            context.Response.Redirect("/vismaconnect/loginfailed");
+            return Task.CompletedTask;
+        };
+        setup.Cookie.SameSite = SameSiteMode.Lax;
+        setup.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        setup.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    })
+    .AddOpenIdConnect(options =>
+    {
+        // TODO: Handle different environments
+        // For Production, this value is supposed to be: https://connect.visma.com
+        // The value below is for the staging environment
+        string authority = "https://connect.identity.stagaws.visma.com";
+
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.Authority = authority;
+        options.CallbackPath = "/vismaconnect/callback/";
+        options.RemoteSignOutPath = "/vismaconnect/signout/";
+        // options.ClientId = "<Developer Portal Client ID>";
+        // options.ClientSecret = "<Developer Portal Secret>";
+        options.ClientId = "<CLIENT_ID>";
+        options.ClientSecret = "<CLIENT_SECRET>";
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.ResponseMode = OpenIdConnectResponseMode.FormPost;
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        // Scope openid and profile are added by default. Add more scopes if needed.
+        // options.Scope.Add("tenants");
+        // options.ClaimActions.MapJsonKey("tenants", "tenants");
+        options.Events.OnRedirectToIdentityProvider = context =>
+        {
+            string? tenantId;
+            try
+            {
+                tenantId = context.Request.Query["tenantid"];
+            }
+            catch (System.Exception)
+            {
+                return Task.CompletedTask;
+            }
+            // Small validation for tenantId
+            if (!string.IsNullOrWhiteSpace(tenantId) && tenantId.Length > 10)
+            {
+                context.Response.Cookies.Append(
+                    "tenantid",
+                    tenantId ?? "",
+                    new Microsoft.AspNetCore.Http.CookieOptions
+                    {
+                        Expires = DateTime.Now.AddMinutes(60)
+                    }
+                );
+                context.ProtocolMessage.Parameters.Add("tenant_hint", tenantId);
+            }
+            return Task.CompletedTask;
+        };
+    });
+
 // Add the files that should be served when visiting the root path of the application
 builder.Services.AddSpaStaticFiles(config =>
 {
